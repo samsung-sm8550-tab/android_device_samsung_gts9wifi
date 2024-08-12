f644d273cee02c899f8fcfea89da3dceab186b09
Davide Garberi <dade.garberi@gmail.com>
2024-08-12 07:44:54 -0700

    gts9wifi: Add missing handheld core permissions
    * This gets automatically included when using the
      full_base_telephony makefile, while it doesn't
      with just full_base.
    
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -29,6 +29,10 @@ PRODUCT_PACKAGES += \
     Gts9wifiSettingsResTarget \
     Gts9wifiSystemUIResTarget
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     init.udfps.rc
