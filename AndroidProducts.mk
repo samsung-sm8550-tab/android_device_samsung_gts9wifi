e9a91b881d84f25091282068c4d4bfa1ecaac41d
Michael Bestas <mkbestas@lineageos.org>
2024-09-29 00:49:53 -0700

    gts9wifi: Drop `COMMON_LUNCH_CHOICES`
    Change-Id: I0509bb0902b75416ef69ef5b1b98936aa876052e
    Signed-off-by: Kai Jones <19639817+488315@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,8 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_gts9wifi.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_gts9wifi-eng \
-    lineage_gts9wifi-user \
-    lineage_gts9wifi-userdebug
