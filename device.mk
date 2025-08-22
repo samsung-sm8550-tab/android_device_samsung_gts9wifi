c666811091593b908942ffadebbcc81822d88b8e
Kai Jones <kvjones0243@gmail.com>
2025-08-22 14:21:20 -0700

    gts9wifi: build `spen-service`
    Change-Id: I2bcfc3ca6fe1fc6d4f0db1226275d5e29a5e4f9d

--- device.mk
+++ device.mk
@@ -53,7 +53,8 @@ PRODUCT_PACKAGES += \
 # SamsungParts
 PRODUCT_PACKAGES += \
    SamsungParts \
-   SPenActions
+   SPenActions \
+   vendor.samsung.hardware.spen-service
 
 # Tablet core
 PRODUCT_COPY_FILES += \
